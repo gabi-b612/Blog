7cff96a6f388a306567bc4d1019f56e5196a6ff1
gabi-b612 <20km095@esisalama.org>
2022-10-18 17:40:15 +0200

    initial js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 // Grab elements
+const selectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+};
 
 //Nav styles on scroll
+const scrollHeader = () =>{
+    const navbarElement = selectElement('#header');
+    if(this.scrollY >= 15) {
+        navbarElement.classList.add('activated');
+    } else {
+        navbarElement.classList.remove('activated');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 // Open menu & search pop-up
 
